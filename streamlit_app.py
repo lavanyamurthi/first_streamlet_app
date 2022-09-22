c5eb0956ab9aea904c0807527f1cd699a9d88dc8
lavanyamurthi <53431304+lavanyamurthi@users.noreply.github.com>
2022-09-22 11:10:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('Thanks for adding', add_my_fruit)
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+f_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 # ##streamlit.text(fruityvice_response.json())
 
 # # write your own comment -what does the next line do? 
